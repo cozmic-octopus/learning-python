29123c2237f7f49fd39d284afc09646a9fc077f8
Emilia Grablewska <emilia.grablewska@tutanota.com>
2024-07-01 17:54:18 +0200

    Add styling changes

--- universities.py
+++ universities.py
@@ -20,13 +20,14 @@ def median(single_list):
     return single_list[len(single_list)//2]
 
 
-universities = [['California Institute of Technology', 2175, 37704],
-['Harvard', 19627, 39849],
-['Massachusetts Institute of Technology', 10566, 40732],
-['Princeton', 7802, 37000],
-['Rice', 5879, 35551],
-['Stanford', 19535, 40569],
-['Yale', 11701, 40500]
+universities = [
+    ['California Institute of Technology', 2175, 37704],
+    ['Harvard', 19627, 39849],
+    ['Massachusetts Institute of Technology', 10566, 40732],
+    ['Princeton', 7802, 37000],
+    ['Rice', 5879, 35551],
+    ['Stanford', 19535, 40569],
+    ['Yale', 11701, 40500]
 ]
 
 stds, fees = enrollment_stats(universities)
